f73fe3017c41a08d6b241ea582aead5d7c26a025
JJRasmussen <123456789+jeppe-juul@hotmail.com>
2024-11-10 00:03:15 +0100

    add functionallity and improve styling

--- library.js
+++ library.js
@@ -7,16 +7,42 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+function addBook(title, author, pages, read){
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
 function placeTitle(book, selector){
     const createBookTitle = document.createElement("h2");
     createBookTitle.textContent = book.title;
     selector.appendChild(createBookTitle);
 }
 
-function placeInfo(book, info, infoselector){
+function placeInfo(book, info, infoselector, type){
     const createBookInfo = document.createElement("li");
+    createBookInfo.setAttribute("id", type + myLibrary.length.toString());
+    
+    if (type === "pages"){
+        createBookInfo.textContent = info + " pages";
+    }
+    else{
     createBookInfo.textContent = info;
+    };
     infoselector.appendChild(createBookInfo);
+}
+
+function createDeleteButton(cardSelector, buttonPlacement){
+    const createDeleteButton = document.createElement("button");
+    createDeleteButton.setAttribute("type", "button");
+    createDeleteButton.setAttribute("class", "cardButton");
+    createDeleteButton.setAttribute("id", "deleteButton" + myLibrary.length.toString());
+    createDeleteButton.textContent = "Delete Book";
+    
+    createDeleteButton.addEventListener("click", () => {
+        cardSelector.remove();
+    })
+
+    buttonPlacement.appendChild(createDeleteButton);
 }
 
 function updateLibrary(book){
@@ -34,22 +60,82 @@ function updateLibrary(book){
 
     const createBookInfo = document.createElement("div");
     createBookInfo.setAttribute("class", "bookInfo");
+    createBookInfo.setAttribute("id", "bookInfo" + myLibrary.length.toString());
     cardSelector.appendChild(createBookInfo);
 
-    placeInfo(book, book.author,cardSelector);
-    placeInfo(book, book.pages,cardSelector);
-    placeInfo(book, book.read,cardSelector);
+    const targetBookInfo = document.querySelector("#bookInfo" + myLibrary.length.toString());
 
+    placeInfo(book, book.author,targetBookInfo, "author");
+    placeInfo(book, book.pages,targetBookInfo, "pages");
+    placeInfo(book, book.read,targetBookInfo, "read");
+
+    const createButtonDiv = document.createElement("div");
+    createButtonDiv.setAttribute("class", "bookButtons");
+    createButtonDiv.setAttribute("id", "btnDiv" + myLibrary.length.toString());
+    cardSelector.appendChild(createButtonDiv);
+
+    createDeleteButton(cardSelector, createButtonDiv);
+
+    const createReadButton = document.createElement("button");
+    createReadButton.setAttribute("type", "button");
+    createReadButton.setAttribute("class", "cardButton");
+    createReadButton.setAttribute("id", "readButton" + myLibrary.length.toString());
+    createReadButton.textContent = "Change read status";
     
+    const readInfo = document.querySelector("#" + "read" + myLibrary.length.toString());
+
+    createReadButton.addEventListener("click", () => {
+
+        if (book.read === "Read"){
+            book.read = "Not read"
+            readInfo.textContent = "Not read"
+        }
+        else {
+            book.read = "Read"
+            readInfo.textContent = "Read"
+        }
+        })
+
+    createButtonDiv.appendChild(createReadButton);
+
 }
 
 function addBookToLibrary(title, author, pages, read) {
-
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-
-    updateLibrary(newBook)
+    addBook(title, author, pages, read);
+    updateLibrary(myLibrary[myLibrary.length - 1]);
 }
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read");
 addBookToLibrary("The Hobbit Two, The Hobbiting", "J.R.R Tolkien", 69, "not read");
+
+const dialog = document.querySelector(".formDialog");
+const showDialogButton = document.querySelector(".showDialog");
+const closeDialog = document.querySelector(".closeDialogButton");
+
+showDialogButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+closeDialog.addEventListener("click", () => {
+    dialog.close();
+    bookForm.reset();
+});
+
+const addBookButton = document.querySelector("#addBookButton");
+const bookForm = document.querySelector("#bookForm");
+
+function addBookButtonClick(event){
+    event.preventDefault();
+
+    let title = document.forms["bookForm"]["title"].value
+    let author = document.forms["bookForm"]["author"].value
+    let pages = document.forms["bookForm"]["pages"].value
+    let read = document.forms["bookForm"]["read"].value
+
+    addBookToLibrary(title, author, pages, read);
+    dialog.close();
+    bookForm.reset();
+};
+
+addBookButton.addEventListener("click", addBookButtonClick);
+
